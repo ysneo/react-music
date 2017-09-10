4de8389601df875dddc9065e761f27e6caf2dad2
ysneo <jinchn728@gmail.com>
2017-09-10 09:26:17 +0800

    set up automation of webpack

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './hello.css'
 // class Hello extends React.Component {
 //     re
 // }
 function Hello(props) {
-  return <h1>Hello, World. Use jsx </h1>
+  return <h1>Hello, World. Use jsx .</h1>
 }
 export default Hello
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 const config = {
   entry: './src/index.js',
@@ -7,17 +10,37 @@ const config = {
     filename: 'bundle.js'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    // contentBase: './dist',
+    compress: true,
+    port: 4000,
+    hot: true
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'React Music',
+      template: './index.html',
+      output: './dist/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
 module.exports = config
